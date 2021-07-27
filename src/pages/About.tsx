57d9ff9f8d7397f02481f8f6cd2e0bf1b800fc71
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-27 15:49:31 -0500

    misc: add content and styles for about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,46 +3,113 @@ import { CTX } from "../context"
 import { Breadcrumbs, Link } from "../components"
 import styled from "styled-components"
 import { Row, Col } from "antd"
+import { primary_color } from "../theme/colors"
+
+const VideoContainer = styled.div`
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px;
+    height: 0;
+    overflow: hidden;
+`
+
+const YoutubePlayer = styled.iframe`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 32px 32px 32px 0;
+`
+
+const CallToActionBox = styled.div`
+    border: 2px solid hsl(213 65% 19%);
+    border-radius: 5px;
+    margin: 32px 0;
+    padding: 16px;
+    text-align: center;
+`
+
+const CallToActionHeader = styled.h2`
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: ${primary_color};
+`
 
 const LinkStyles = {
     color: "white",
+    fontSize: "1.125rem",
     fontWeight: 500,
     backgroundColor: "#048392",
     padding: "4px 8px",
     borderRadius: "5px",
 }
 
+const HeaderOne = styled.h1`
+    font-size: 2rem;
+`
+
+const HeaderTwo = styled.h2`
+    font-size: 1.5rem;
+    color: ${primary_color};
+`
+
+const BodyText = styled.p`
+    font-size: 1.125rem;
+`
+
 export const About = () => {
+    const { parse } = useContext(CTX)
+    const { URL_PATH } = parse()
+
     return (
         <>
+            <Breadcrumbs path={URL_PATH} />
             <Row>
                 <Col lg={16} sm={24}>
-                    video here
+                    <VideoContainer>
+                        <YoutubePlayer
+                            src="https://www.youtube.com/embed/QHQCbglb3FM"
+                            title="YouTube video player for Census Academy"
+                        ></YoutubePlayer>
+                    </VideoContainer>
                 </Col>
                 <Col lg={8} sm={24}>
-                    call to action here
+                    <CallToActionBox>
+                        <CallToActionHeader>
+                            Meet one on one with our Census Academy team
+                        </CallToActionHeader>
+                        <BodyText>
+                            Get personalized support and insider information by setting up an
+                            appointment with our experienced data analysts and technicians. This
+                            service is free for all interested data users.
+                        </BodyText>
+                        <a href="mailto: census.academy@census.gov" style={LinkStyles}>
+                            Request Data Training
+                        </a>
+                    </CallToActionBox>
                 </Col>
             </Row>
             <div>
-                <h1>About Census Academy</h1>
-                <h2>Mission Statement</h2>
-                <p>
+                <HeaderOne>About Census Academy</HeaderOne>
+                <HeaderTwo>Mission Statement</HeaderTwo>
+                <BodyText>
                     The Census Academy team is your virtual hub for learning data skills. We are
                     made of a team that aims to give free, accessible data resources, information,
                     and courses to all level of data users.
-                </p>
-                <h2>Our Materials</h2>
-                <p>
+                </BodyText>
+                <HeaderTwo>Our Materials</HeaderTwo>
+                <BodyText>
                     Our team of experts is excited to share with you their favorite tips and tricks
                     about how to access and use Census Bureau Data. Our materials will introduce you
                     to various concepts and techniques to improve your ability to navigate our
                     website and use our data-access tools.
-                </p>
-                <p>
+                </BodyText>
+                <BodyText>
                     Drop us a line at{" "}
                     <a href="mailto: census.academy@census.gov">census.academy@census.gov</a> to let
                     use us know what you think.
-                </p>
+                </BodyText>
             </div>
         </>
     )
