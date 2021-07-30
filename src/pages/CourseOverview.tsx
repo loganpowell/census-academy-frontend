f53154b4f01a16011299db43dd1198e995ca43ae
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-30 14:05:16 -0500

    misc: mock up course overview pg w dummy links

--- src/pages/CourseOverview.tsx
+++ src/pages/CourseOverview.tsx
@@ -5,30 +5,71 @@ import { unified } from "unified"
 import parse from "remark-parse"
 import remark2react from "remark-react"
 import YouTube from "react-youtube"
-import { Breadcrumbs } from "../components"
+import { Row, Col, Button } from "antd"
+import { Breadcrumbs, Link } from "../components"
 import { primary_color } from "../theme/colors"
 
 const Wrapper = styled.div`
-    width: 100%;
-    max-width: 650px;
+    margin: 16px 64px;
 `
 
-const ContentHeading = styled.h1`
-    font-size: 1.75rem;
-    color: ${primary_color};
+const StyledHero = styled(Col)`
+    background: rgb(2, 0, 36);
+    background: linear-gradient(0deg, rgba(2, 0, 36, 1) 0%, rgba(4, 131, 146, 1) 80%);
+    padding: 48px 32px;
 `
 
-const DateText = styled.p`
+const HeroHeader = styled.h1`
+    font-size: 2rem;
+    font-weight: 500;
+    margin: 0;
+    margin-left: 32px;
+    color: white;
+`
+
+const HeroSubHeading = styled.h2`
+    font-size: 1.5rem;
+    font-weight: 300;
+    margin: 0;
+    margin-left: 32px;
+    color: white;
+`
+
+const StyledParagraph = styled.p`
+    display: inline-block;
     font-size: 1.125rem;
-    margin: 8px 0;
+    font-weight: bold;
 `
+
+const StyledSpan = styled.span`
+    display: inline-block;
+    margin: 0 4px;
+    font-size: 1.125rem;
+`
+
+const CourseInfo = styled.div`
+    text-align: center;
+    padding-top: 16px;
+    background: var(--amplify-primary-tint);
+    color: white;
+`
+
+const LinkStyles = {
+    color: "white",
+    fontSize: "1rem",
+    fontWeight: 500,
+    backgroundColor: "#048392",
+    margin: "16px",
+    padding: "8px 16px",
+    borderRadius: "5px",
+}
 
 export const CourseOverview = ({ data }) => {
     console.log("Course data:", data)
     const context = useContext(CTX)
     const { URL_PATH } = context.parse()
 
-    const { T_BODY, T_OG_TITLE, A_VIDEO, date } = data
+    const { T_OG_DESCRIPTION, T_BODY, T_OG_TITLE, A_VIDEO, date } = data
     const formattedDate = new Intl.DateTimeFormat("en-US", {
         year: "numeric",
         month: "long",
@@ -36,17 +77,43 @@ export const CourseOverview = ({ data }) => {
     }).format(Date.parse(date))
     const title = T_OG_TITLE?.content
     const body = unified().use(parse).use(remark2react).processSync(T_BODY?.content).result
-    const id = A_VIDEO?.content.split("/").pop()
 
     return (
         <div style={{ margin: "0px 192px" }}>
             <Breadcrumbs path={URL_PATH} />
-            <ContentHeading>{title}</ContentHeading>
+            <Row>
+                <StyledHero lg={24} md={24} sm={24}>
+                    <HeroHeader>{title}</HeroHeader>
+                    <HeroSubHeading>{T_OG_DESCRIPTION?.content}</HeroSubHeading>
+                    <div style={{ marginLeft: "16px", marginTop: "24px" }}>
+                        <Link href="#" style={LinkStyles}>
+                            Start Course
+                        </Link>
+                        <Link href="#" style={LinkStyles}>
+                            Save for Later
+                        </Link>
+                    </div>
+                </StyledHero>
+                {/* in the mockup there's an SVG here */}
+                {/* <StyledHero lg={6} md={24} sm={24}>
+                    icon
+                </StyledHero> */}
+            </Row>
+            <CourseInfo>
+                {/* How should we store this information -- separate assets on the
+                node? What type should we give those assets? */}
+                <StyledParagraph>Skill level: {"Beginner"}</StyledParagraph>
+                <StyledSpan>|</StyledSpan>
+                <StyledParagraph>Duration: {"0-1 Hour"}</StyledParagraph>
+                <StyledSpan>|</StyledSpan>
+                <StyledParagraph>Content: {"14 Videos"}</StyledParagraph>
+                <StyledSpan>|</StyledSpan>
+                <StyledParagraph>Rewards: {"Certificate and 2 Badges"}</StyledParagraph>
+            </CourseInfo>
             <Wrapper>
-                <YouTube videoId={id} />
+                <h2 style={{ fontWeight: 500 }}>Course Description</h2>
+                <p>{body}</p>
             </Wrapper>
-            <DateText>{formattedDate}</DateText>
-            <p>{body}</p>
         </div>
     )
 }
