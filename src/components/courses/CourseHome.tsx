d62eed82b6725f78da2fa24080f65d0f70f6418d
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-08-03 11:50:07 -0500

    refactor: lift antd layout components from components to pages
    Lift `antd` Layout components (Layout and Sider) to parent
    `Courses.tsx`
    
    This way, components related to courses only need to focus on the
    body of the page and are not concerned with the markup around them

--- src/components/courses/CourseHome.tsx
+++ src/components/courses/CourseHome.tsx
@@ -1,12 +1,9 @@
-import React, { useContext } from "react"
+import React from "react"
 import styled from "styled-components"
-import { CTX } from "../../context"
-import { Layout, Menu, Collapse } from "antd"
-import { Breadcrumbs, Link } from ".."
+import { Collapse } from "antd"
+import { Link } from ".."
 
 const { Panel } = Collapse
-const { SubMenu } = Menu
-const { Content, Sider } = Layout
 
 const StyledItem = styled.li`
     list-style-type: circle;
@@ -24,70 +21,43 @@ const LinkStyles = {
 }
 
 export const CourseHome = ({ courseTitle, modules, courseId }) => {
-    const context = useContext(CTX)
-    const { URL_PATH } = context.parse()
-
     // TODO make this page mobile friendly!!!
     // the antd Layout component is not mobile friendly!!
     return (
-        <Layout style={{ margin: "32px" }}>
-            <Sider style={{ background: "white" }}>
-                {/* populate sidebar with sections and navigation */}
-                <Menu mode="inline">
-                    {/* we do not want to use asset ids here,
-                        ultimately we want to re-work C_COURSE nodes data structure
-                        to have children node, which we then render their
-                        titles, instead of IDs
-                    */}
-                    {modules.map(module => (
-                        <SubMenu key={module.id} title={module.id}>
-                            {module.node.assets?.items.map(asset => (
-                                <Menu.Item key={asset.id}>
-                                    <Link href={`courses/${courseId}/module/${module.node.id}`}>
-                                        {asset.id}
-                                    </Link>
-                                </Menu.Item>
-                            ))}
-                        </SubMenu>
-                    ))}
-                </Menu>
-            </Sider>
-            <Content style={{ marginLeft: "16px" }}>
-                {/* populate content with overview of each submodule */}
-                <Breadcrumbs path={URL_PATH} />
-                <h1>{courseTitle}</h1>
-                <Collapse>
-                    {modules.map(module => (
-                        <Panel
-                            key={module.node.id}
-                            // for the header currently just using node id
-                            // however what would likely be a better choice is
-                            // using a T_OG_TITLE asset on the node
-                            header={module.node.id}
-                        >
-                            <p>What this module will cover:</p>
-                            <ul>
-                                {/* we should not be using the asset id.
+        <>
+            {/* populate content with overview of each submodule */}
+            <h1>{courseTitle}</h1>
+            <Collapse>
+                {modules.map(module => (
+                    <Panel
+                        key={module.node.id}
+                        // for the header currently just using node id
+                        // however what would likely be a better choice is
+                        // using a T_OG_TITLE asset on the node
+                        header={module.node.id}
+                    >
+                        <p>What this module will cover:</p>
+                        <ul>
+                            {/* we should not be using the asset id.
                                     ideally, this would actually be another list
                                     of connected nodes representing the
                                     sub modules on this section.
                                     and on the node itself, it should have a T_OG_TITLE
                                     asset, which we can use instead of asset id
                                 */}
-                                {module.node.assets?.items.map(asset => (
-                                    <StyledItem key={asset.id}>{asset.id}</StyledItem>
-                                ))}
-                            </ul>
-                            <Link
-                                href={`courses/${courseId}/module/${module.node.id}`}
-                                style={LinkStyles}
-                            >
-                                Go to Module
-                            </Link>
-                        </Panel>
-                    ))}
-                </Collapse>
-            </Content>
-        </Layout>
+                            {module.node.assets?.items.map(asset => (
+                                <StyledItem key={asset.id}>{asset.id}</StyledItem>
+                            ))}
+                        </ul>
+                        <Link
+                            href={`courses/${courseId}/module/${module.node.id}`}
+                            style={LinkStyles}
+                        >
+                            Go to Module
+                        </Link>
+                    </Panel>
+                ))}
+            </Collapse>
+        </>
     )
 }
--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react"
-import { CourseHome, CourseContent } from "../components"
-import { node } from "cope-client-utils"
-import { EdgeType } from "cope-client-utils/lib/graphql/API"
+import React, { useEffect, useContext } from "react"
+import { CTX } from "../context"
+import { CourseHome, CourseContent, Breadcrumbs, Link } from "../components"
+import { Layout, Menu } from "antd"
+
+const { Content, Sider } = Layout
+const { SubMenu } = Menu
 
 export const Course = ({ data }) => {
+    const context = useContext(CTX)
+    const { URL_PATH } = context.parse()
     const { T_OG_TITLE, connectedNodes, courseId, path } = data
     let displayComponent
     console.log("data", data)
-    console.log("current path:", path)
 
     switch (path.pop()) {
         case "home":
@@ -23,5 +27,33 @@ export const Course = ({ data }) => {
             displayComponent = <CourseContent />
     }
 
-    return displayComponent
+    return (
+        <Layout style={{ margin: "32px" }}>
+            <Sider style={{ background: "white" }}>
+                {/* populate sidebar with sections and navigation */}
+                <Menu mode="inline">
+                    {/* we do not want to use asset ids here,
+                        ultimately we want to re-work C_COURSE nodes data structure
+                        to have children node, which we then render their
+                        titles, instead of IDs
+                    */}
+                    {connectedNodes.map(module => (
+                        <SubMenu key={module.id} title={module.id}>
+                            {module.node.assets?.items.map(asset => (
+                                <Menu.Item key={asset.id}>
+                                    <Link href={`courses/${courseId}/module/${module.node.id}`}>
+                                        {asset.id}
+                                    </Link>
+                                </Menu.Item>
+                            ))}
+                        </SubMenu>
+                    ))}
+                </Menu>
+            </Sider>
+            <Content style={{ marginLeft: "16px" }}>
+                <Breadcrumbs path={URL_PATH} />
+                {displayComponent}
+            </Content>
+        </Layout>
+    )
 }
